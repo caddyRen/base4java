164be5e8477ebd746d251cd85a287438845b1e4f
caddy <18819284248@163.com>
2020-10-22 23:50:57 +0800

    feet: 优化代码
    - java8写法

--- design_pattern/src/main/java/indi/ikun/spring/designpattern/ppm/MaterialBill.java
+++ design_pattern/src/main/java/indi/ikun/spring/designpattern/ppm/MaterialBill.java
@@ -24,6 +24,16 @@ public class MaterialBill {
     private Date issueDate;
 
     /**
+     * 是否由调拨单生成
+     */
+    private String isFromMovement;
+
+    /**
+     * 领料类型
+     */
+    private Integer issueType;
+
+    /**
      * 项目信息
      */
     private ProjectInfo projectInfo;
@@ -34,57 +44,17 @@ public class MaterialBill {
     private TakingDept takingDept;
 
     /**
-     * 是否由调拨单生成
-     */
-    private String isFromMovement;
-
-    /**
      * 领料人id
      */
-    private String receiverId;
-    /**
-     * 领料人
-     */
-    private String receiver;
-    /**
-     * 实际领料人身份证号
-     */
-    private String receiverIDCard;
-
+    private Receiver receiver;
 
     /**
      * 实际领料人id
      */
-    private String actualReceiverId;
-    /**
-     * 实际领料人
-     */
-    private String actualReceiver;
-    /**
-     * 实际领料人身份证号
-     */
-    private String actualReceiverIDCard;
-
-    /**
-     * 领料类型
-     */
-    private Integer issueType;
+    private ActualReceiver actualReceiver;
 
     /**
      * 制单人
      */
-    private String creator;
-    /**
-     * 制单人id
-     */
-    private String creatorId;
-    /**
-     * 制单日期
-     */
-    private Date creatorDate;
-
-    /**
-     * 领料申请单id
-     */
-    private String issueReqID;
+    private Creator creator;
 }
--- design_pattern/src/main/java/indi/ikun/spring/designpattern/ppm/MaterialDetail.java
+++ design_pattern/src/main/java/indi/ikun/spring/designpattern/ppm/MaterialDetail.java
@@ -7,6 +7,16 @@ package indi.ikun.spring.designpattern.ppm;
 public class MaterialDetail extends MaterialBill {
 
     /**
+     * 领料单物资详情id
+     */
+    private String issueItemId;
+
+    /**
+     * 物资id
+     */
+    private String materialId;
+
+    /**
      * 物资名称
      */
     private String materialName;
@@ -19,20 +29,38 @@ public class MaterialDetail extends MaterialBill {
     /**
      * 计量单位
      */
-    private String unitName;
-
-    /**
-     * 计量单位id
-     */
-    private String unitId;
+    private Unit unit;
 
     /**
      * 申请领用数量
      */
     private Double reqIssueQty;
 
+    /**
+     * 领料申请单id *************确认领料单里的字段是否为同一字段
+     */
+    private String issueReqID;
 
-/*************    以下是会变化的量   ****************/
+    /**
+     * 领料申请明细ID
+     */
+    private String issueReqItemId;
+
+    /**
+     * 跨账调拨单ID
+     */
+    private String movementItemId;
+    /**
+     * 跨账调拨单明细ID
+     */
+    private String fromMovementItemId;
+
+
+
+/*************    以下是会变化的量
+ * 使用享元模式共享状态
+ *
+ * ****************/
 
     /**
      * 物资状态
