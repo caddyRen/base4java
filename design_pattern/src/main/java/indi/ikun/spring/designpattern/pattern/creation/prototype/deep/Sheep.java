b60d0191e081fdb87df42c7bbf2b543b83548875
renqiankun <18819284248@163.com>
2020-11-02 16:06:23 +0800

    bugfix: 修复序列化bug
    - 原型模式
      - 修复序列化深拷贝try-catch错误写法

--- design_pattern/src/main/java/indi/ikun/spring/designpattern/pattern/creation/prototype/deep/Sheep.java
+++ design_pattern/src/main/java/indi/ikun/spring/designpattern/pattern/creation/prototype/deep/Sheep.java
@@ -10,7 +10,7 @@ import java.io.*;
  * @date 2020-02-11 14:07:57
  */
 public class Sheep implements Cloneable, Serializable {
-    private static final long serailVersionUID = 1L;
+    private static final long SERAILVERSIONUID = 1L;
     String name;
     String color;
     int age;
@@ -20,17 +20,40 @@ public class Sheep implements Cloneable, Serializable {
     public Object deepClone() {
         Sheep object = null;
         //创建流对象
-        try (ByteArrayOutputStream bos = new ByteArrayOutputStream();//序列化
-             ObjectOutputStream oos = new ObjectOutputStream(bos);//序列化
-             ByteArrayInputStream bis = new ByteArrayInputStream(bos.toByteArray());//反序列化
-             ObjectInputStream ois = new ObjectInputStream(bis))
-        {
 
+        ByteArrayInputStream bis=null;
+        ObjectInputStream ois=null;
+
+        try (
+            //序列化
+            ByteArrayOutputStream bos = new ByteArrayOutputStream();
+            //序列化
+            ObjectOutputStream oos = new ObjectOutputStream(bos)
+        ) {
             //当前这个对象以对象流的方式输出
             oos.writeObject(this);
+            //反序列化
+            bis = new ByteArrayInputStream(bos.toByteArray());
+            ois = new ObjectInputStream(bis);
             object = (Sheep) ois.readObject();
         } catch (Exception e) {
             e.printStackTrace();
+        }finally {
+            if(bis!=null){
+                try {
+                    bis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if(ois!=null){
+                try {
+                    ois.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
         }
         return object;
     }
