cf956ab8ae1a25434983fe6eb1564e805157e66f
caddy <18819284248@163.com>
2020-12-08 22:52:55 +0800

    feat: huffmanTree
    - huffmanTree
      - 文件压缩与解压缩

--- dsaa/src/main/java/indi/ikun/spring/dsaa/tree/huffman/HuffmanCoding.java
+++ dsaa/src/main/java/indi/ikun/spring/dsaa/tree/huffman/HuffmanCoding.java
@@ -2,6 +2,7 @@ package indi.ikun.spring.dsaa.tree.huffman;
 
 import lombok.ToString;
 
+import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.util.*;
 
@@ -59,63 +60,130 @@ public class HuffmanCoding {
     static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) {
-        testBit();
-        String str = "i like like like java do you like a java java java like java like you";
-        byte[] data = str.getBytes(StandardCharsets.UTF_8);
-        byte[] zip = huffmanZip(data);
+//        testBit();
+//        String str = "i like like like java do you like a java java java like java like you";
+//        byte[] data = str.getBytes(StandardCharsets.UTF_8);
+//        byte[] zip = huffmanZip(data);
         //解码
-        byte[] decode = decode(zip);
-        System.err.println(Arrays.toString(decode));
-        String result=new String(decode);
-        System.err.println(result);
+//        byte[] decode = decode(zip,huffmanCodes);
+//        System.err.println(Arrays.toString(decode));
+//        String result = new String(decode);
+//        System.err.println(result);
+
+        //图片压缩测试当图片颜色很丰富压缩率不高，当图片很单调时压缩率还可以
+        //色彩丰富
+//        String srcFile="/home/caddy/Pictures/bg/KUN_1858.NEF";//25M
+//        String dstFile="/home/caddy/DSC_0099.zip";//25M
+//        zipFile(srcFile,dstFile);
+        //单调图片
+//        String srcFile2="/home/caddy/Pictures/20201203_bak/KUN_1858.NEF"; //43M
+//        String dstFile2="/home/caddy/KUN_1858.NEF.zip";//29M
+//        zipFile(srcFile2,dstFile2);
+
+        //文件解压缩
+//        String srcFile = "/home/caddy/KUN_1858.NEF.zip";//29M
+//        String dstFile = "/home/caddy/KUN_1858.NEF";//43M
+//        unzipFile(srcFile, dstFile);
+
+
     }
 
     /**
+     * @param srcFile 待解压缩文件路径
+     * @param dstFile 解压缩后文件路径
+     */
+    public static void unzipFile(String srcFile, String dstFile) {
+
+        try (
+                InputStream is = new FileInputStream(srcFile);
+                ObjectInputStream ois = new ObjectInputStream(is);
+                OutputStream os=new FileOutputStream(dstFile)
+        ) {
+            byte[] huffmanBytes =(byte[]) ois.readObject();
+            Map<Byte, String> huffmanCode=(Map<Byte, String>)ois.readObject();
+            byte[] decode = decode(huffmanBytes,huffmanCode);
+            //写数据到文件
+            os.write(decode);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+
+    }
+
+
+    /**
+     * 压缩文件
+     *
+     * @param srcFile 待压缩文件路径
+     * @param dstFile 压缩后文件路径
+     */
+    public static void zipFile(String srcFile, String dstFile) {
+        try (
+                FileInputStream is = new FileInputStream(srcFile);
+                FileOutputStream os = new FileOutputStream(dstFile);
+                ObjectOutputStream oos = new ObjectOutputStream(os)
+        ) {
+            byte[] bytes = new byte[is.available()];
+            is.read(bytes);
+            //压缩文件
+            byte[] huffmanZip = huffmanZip(bytes);
+            //以  对象流  的方式写入huffman编码，是为了以后恢复源文件使用
+            oos.writeObject(huffmanZip);
+            oos.writeObject(huffmanCodes);//huffman编码也要写入压缩文件，解码时需要使用
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    /**
      * zip 的最后一个不需要补位,且最后一位肯定是1开头，因为最后一位生成的时候肯定不会是0开头
+     *
      * @param zip
      * @return 原来字符串对应的byte数组
      */
-    private static byte[] decode(byte[] zip){
-        boolean flag=true;
-        StringBuilder builder=new StringBuilder();
-        for (int i=0;i<zip.length;i++){
-            if(i==zip.length-1){
-                flag=false;
+    private static byte[] decode(byte[] zip,Map<Byte, String> huffmanCode) {
+        boolean flag = true;
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < zip.length; i++) {
+            if (i == zip.length - 1) {
+                flag = false;
             }
-            builder.append(byteToBitString(zip[i],flag));
+            builder.append(byteToBitString(zip[i], flag));
         }
-        System.err.println("使用huffmanCodes解码字符串为\n"+builder.toString());
+//        System.err.println("使用huffmanCodes解码字符串为\n"+builder.toString());
 
-        Map<String,Byte> map=new HashMap<>();
-        for(Map.Entry<Byte,String> entry:huffmanCodes.entrySet()){
-            map.put(entry.getValue(),entry.getKey());
+        Map<String, Byte> map = new HashMap<>();
+        for (Map.Entry<Byte, String> entry : huffmanCode.entrySet()) {
+            map.put(entry.getValue(), entry.getKey());
         }
-        String ss="01234567";
-        System.err.println(ss.substring(1,3));
-        System.err.println(builder.substring(1,3));
+        String ss = "01234567";
+//        System.err.println(ss.substring(1,3));
+//        System.err.println(builder.substring(1,3));
         //遍历得到原始字符串的byte[]
-        List<Byte> list=new ArrayList<>();
-        for (int i=0;i<builder.length();){
-            int count=1;
-            boolean bol=true;
-            Byte b=null;
-            while (bol){
+        List<Byte> list = new ArrayList<>();
+        for (int i = 0; i < builder.length(); ) {
+            int count = 1;
+            boolean bol = true;
+            Byte b = null;
+            while (bol) {
                 //public String substring(int start, int end)  start ,end是下标 [start,end) 包含start ，不包含end
                 //abcdef,substring(1,3)==bc
-                String str=builder.substring(i,i+count);
-                b=map.get(str);
-                if(b==null){
+                String str = builder.substring(i, i + count);
+                b = map.get(str);
+                if (b == null) {
                     count++;
-                }else{
-                    bol=false;
+                } else {
+                    bol = false;
                 }
             }
             list.add(b);
-            i+=count;
+            i += count;
         }
-        byte by[]=new byte[list.size()];
+        byte by[] = new byte[list.size()];
         for (int i = 0; i < list.size(); i++) {
-            by[i]=list.get(i);
+            by[i] = list.get(i);
         }
         return by;
     }
@@ -123,7 +191,7 @@ public class HuffmanCoding {
 
     /**
      * 将byte转成二进制的字符串
-     *
+     * <p>
      * 如果原数据的长度不是8的倍数，即最后一位的长度不是8位（不够8位肯定是个正数，因为满8位且第一位是1才会是负数）
      * 不能进行补位，否则解码肯定失败，所以设置一个flag，确定是否补高位
      *
@@ -153,18 +221,18 @@ public class HuffmanCoding {
         //获取huffmanTree
         DataNode root = createHuffmanTree(getNodes(data));
         //前序遍历huffmanTree
-        System.err.println("前序遍历huffmanTree");
-        preOrder(root);
+//        System.err.println("前序遍历huffmanTree");
+//        preOrder(root);
         //获取huffmanCodes
         //{32=01, 97=100, 100=11000, 117=11001, 101=1110, 118=11011, 105=101, 121=11010, 106=0010, 107=1111, 108=000, 111=0011}
         getCodes(root);
-        System.err.println("根据huffmanTree获取的huffman编码为： \n" + huffmanCodes);
-        System.err.println("压缩前byte数组长度=" + data.length);
-        System.err.println("压缩前byte数组\n" + Arrays.toString(data));
+//        System.err.println("根据huffmanTree获取的huffman编码为： \n" + huffmanCodes);
+//        System.err.println("压缩前byte数组长度=" + data.length);
+//        System.err.println("压缩前byte数组\n" + Arrays.toString(data));
         //使用huffmanCodes，编码str
         byte[] zip = zip(data);
-        System.err.println("压缩后byte数组长度=" + zip.length);
-        System.err.println("压缩后byte数组\n" + Arrays.toString(zip));
+//        System.err.println("压缩后byte数组长度=" + zip.length);
+//        System.err.println("压缩后byte数组\n" + Arrays.toString(zip));
         return zip;
     }
 
@@ -184,7 +252,7 @@ public class HuffmanCoding {
             builder.append(huffmanCodes.get(b));
         }
         String str = builder.toString();
-        System.err.println("使用huffmanCodes转换后的机器码字符串为：\n" + str);
+//        System.err.println("使用huffmanCodes转换后的机器码字符串为：\n" + str);
         //将编码后的字符串，转成机器码，补码，反码
         // byte 8位，第一位符号位不动 0正1负
         // 1 0101000 求补码(-1)--->1 0100111 求反码---> 1 1011000 转十进制---> -2^6+2^4+2^3=-88
@@ -251,7 +319,7 @@ public class HuffmanCoding {
         while (nodes.size() > 1) {
             //从小到大
             Collections.sort(nodes);
-            System.err.println(nodes);
+//            System.err.println(nodes);
             //取出权值最小的结点
             DataNode leftNode = nodes.get(0);
             //取出权值次小的结点
@@ -285,7 +353,6 @@ public class HuffmanCoding {
             DataNode dataNode = new DataNode(b, count.get(b));
             nodes.add(dataNode);
         }
-//        Collections.sort(nodes);
         return nodes;
     }
 
@@ -297,7 +364,7 @@ public class HuffmanCoding {
         root.postOrder();
     }
 
-    public static void testBit(){
+    public static void testBit() {
         System.err.println("测试byte转二进制字符串");
         //二进制正数和负数 同256 按位或运算，
         // 或运算 该位有1结果即是1
