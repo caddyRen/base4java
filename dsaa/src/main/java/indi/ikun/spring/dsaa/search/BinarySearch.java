baa1af325bfbb952abe7376e471d31f6b8aa5adf
caddy <18819284248@163.com>
2020-12-11 16:51:16 +0800

    feat: 非递归二分查找算法
    - 二分法
      - 查找单个结果
      - 查找重复结果

--- dsaa/src/main/java/indi/ikun/spring/dsaa/search/BinarySearch.java
+++ dsaa/src/main/java/indi/ikun/spring/dsaa/search/BinarySearch.java
@@ -1,10 +1,19 @@
 package indi.ikun.spring.dsaa.search;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
  * 二分查找
+ * 递归版
+ *
+ * 非递归版
+ *
+ * 如果100个数
+ * 时间复杂度=以2为底100的对数
+ * log   100     2^6<100<2^7,最慢7次就可以找到
+ *     2
  *
  * 中值=（left+right）/2
  */
@@ -18,6 +27,13 @@ public class BinarySearch {
         a=b;
         System.err.println(binarySearchPro(1000, a, 0, a.length - 1).toString());
 
+        //非递归版
+        int c[] = {1000, 1000, 1000, 1000, 1000, 1000,1000};
+        System.err.println(binarySearchNoRecur(b,1000));
+        System.err.println(binarySearchNoRecurPro(b,1000));
+        List<Integer> list = binarySearchNoRecurPro(c, 1000);
+        Collections.sort(list);
+        System.err.println(list);
 
     }
 
@@ -88,4 +104,64 @@ public class BinarySearch {
             }
         }else return new ArrayList<>();
     }
+
+    //非递归版二分查找
+
+    /**
+     * 非递归版二分查找算法
+     * @param arr 升序待查找数组 natural ordering ===1,2,3,4
+     * @param target 查找目标
+     * @return 查找目标的下标，-1表示没找到
+     */
+    public static int binarySearchNoRecur(int[] arr,int target){
+        int left=0;
+        int right= arr.length-1;
+        while (left<=right){
+            int mid =(left+right)/2;
+            if(arr[mid]==target){
+                return mid;
+            }else if(arr[mid]>target){
+                right=mid-1;
+            }else {
+                left=mid+1;
+            }
+        }
+        return -1;
+    }
+    /**
+     * 非递归版二分查找算法,重复值
+     * @param arr 升序待查找数组 natural ordering ===1,2,3,4
+     * @param target 查找目标
+     * @return 查找目标的下标，-1表示没找到
+     */
+    public static List<Integer> binarySearchNoRecurPro(int[] arr,int target){
+        List<Integer> result=new ArrayList<>();
+        int left=0;
+        int right= arr.length-1;
+        int temp;
+        while (left<=right){
+            int mid =(left+right)/2;
+            if(arr[mid]==target){
+                temp=mid;
+                result.add(temp);
+                while (temp-1>=0&&target==arr[temp-1]){
+                    result.add(temp-1);
+                    temp-=1;
+                }
+                temp=mid;
+                while (temp+1<arr.length&&target==arr[temp+1]){
+                    result.add(temp+1);
+                    temp+=1;
+                }
+                return result;
+            }else if(arr[mid]>target){
+                right=mid-1;
+            }else {
+                left=mid+1;
+            }
+        }
+        return result;
+    }
+
+
 }
