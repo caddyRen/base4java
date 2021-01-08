661a8c2448361b6108a671443c054b08588b3716
renqiankun <18819284248@163.com>
2021-01-08 14:29:04 +0800

    feat: 使用 & 1 判断奇数偶数

--- base_java/src/main/java/org/bougainvillea/java/basejava/codequality/chapter02/Au.java
+++ base_java/src/main/java/org/bougainvillea/java/basejava/codequality/chapter02/Au.java
@@ -5,10 +5,17 @@ import java.util.Scanner;
 
 /**
  * 21.用偶判断，不用奇判断
+ * 使用 & 与运算 判断奇偶
  */
 public class Au {
     public static void main(String[] args) {
         System.err.println(-1/2);
+        System.err.println(-1&1);
+        System.err.println(-2&1);
+        System.err.println(0&1);
+        System.err.println(1&1);
+        System.err.println(2&1);
+
         System.err.println(remainder(-1,2));
         Scanner input = new Scanner(System.in);
         System.err.println("请输入多个数字判断奇数偶数：");
@@ -19,7 +26,9 @@ public class Au {
 //            String str = i + "->" + (i % 2 == 1 ? "奇" : "偶");
             //用偶判断
             String str = i + "->" + (i % 2 == 0 ? "偶" : "奇");
-            System.err.println(str);
+            String str2 = i + "->" + ((i&1) == 0 ? "偶" : "奇");
+            System.err.println(i+"是"+str);
+            System.err.println(i+"是"+str2);
         }
 
     }
