56300acef2865daecc28705fc929b8b82f6a7dd3
renqiankun <18819284248@163.com>
2021-01-11 15:45:26 +0800

    feat: 自定义ClassLoader

--- jvm/src/main/java/org/bougainvillea/java/jvm/classloader/MyURLClassLoader.java
+++ jvm/src/main/java/org/bougainvillea/java/jvm/classloader/MyURLClassLoader.java
@@ -2,13 +2,23 @@ package org.bougainvillea.java.jvm.classloader;
 
 import java.net.URL;
 import java.net.URLClassLoader;
+import java.net.URLStreamHandlerFactory;
 
 /**
  * 继承URLClassLoader 实现Classloader
  * @author renqiankun
  */
 public class MyURLClassLoader extends URLClassLoader {
+
     public MyURLClassLoader(URL[] urls, ClassLoader parent) {
         super(urls, parent);
+    }
+
+    public MyURLClassLoader(URL[] urls) {
+        super(urls);
+    }
+
+    public MyURLClassLoader(URL[] urls, ClassLoader parent, URLStreamHandlerFactory factory) {
+        super(urls, parent, factory);
     }
 }
